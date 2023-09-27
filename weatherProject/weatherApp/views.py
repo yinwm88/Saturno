2975d0f516133d45c3b271847278e0d05acffcb4
wong <yinlen@ciencias.mx>
2023-09-26 22:27:07 -0600

    actualización

--- weatherProject/weatherApp/views.py
+++ weatherProject/weatherApp/views.py
@@ -6,7 +6,7 @@ import csv
 '''
     Funcion para leer el contenido de un archivo.txt.
 '''
-def readData(rute):
+def read_data(rute):
     with open(rute, 'r') as txt_file:
                 content = txt_file.read()
     return content
@@ -38,7 +38,7 @@ def csv_a_diccionario(archivo):
     Función para ver si la llave se encuentra en el diccionario, 
     con esto revisaremos si la entrada es un ticket o un nombre de una ciudad
 '''
-def esTicket(diccionario, clave):
+def es_ticket(diccionario, clave):
     if clave in diccionario:
         return True
     else:
@@ -50,7 +50,7 @@ def esTicket(diccionario, clave):
 '''
     Funcion para obtener coordenas con GeoCoding.
 '''
-def getCoordenadasGC(city, appid):
+def get_coordenadas_gc(city, appid):
     try:
         geoUrl = "http://api.openweathermap.org/geo/1.0/direct"
         geoParams = {'q': city, 'appid': appid}
@@ -69,44 +69,56 @@ def getCoordenadasGC(city, appid):
 '''
     Función para, dado el ticket, devolver las coordenadas de cada ciudad.
 '''
-def getCoordenadasDS(ticket, archivo):
-    diccionarioTicketsData = csv_a_diccionario(archivo) 
-    clave = ticket
-     
-    latOrigin = diccionarioTicketsData[clave]['origin_latitude']
-    lonOrigin = diccionarioTicketsData[clave]['origin_latitude']
-    latDestination = diccionarioTicketsData[clave]['destination_latitude']
-    lonDestination = diccionarioTicketsData[clave]['destination_latitude']
-     
-    return latOrigin, lonOrigin, latDestination, lonDestination
-
-
-
+def get_coordenadas_ds(ticket, diccionario): 
+    if diccionario  and 'origin_latitude' in diccionario[ticket] and 'origin_longitude'  in diccionario[ticket] and 'destination_latitude' in diccionario[ticket] and 'destination_longitude' in diccionario[ticket]:
+        latO = diccionario[ticket]['origin_latitude']
+        lonO = diccionario[ticket]['origin_longitude']
+        latD = diccionario[ticket]['destination_latitude']
+        lonD = diccionario[ticket]['destination_longitude']
+        return latO, lonO, latD, lonD
+    else:
+        return ticket, "No es un ticket.", None, None
+    
 
 '''
     Función para obtener el clima de una ciudad.
 '''
-def obtenerClima(lat, lon, appid):
-    owUrl = "https://api.openweathermap.org/data/2.5/weather"
-    owParams = {'lat': lat, 'lon': lon, 'appid': appid}
-    owResp = requests.get(url=owUrl, params=owParams)
-    owResp.raise_for_status()
-    djsonOW = owResp.json()
+import requests
 
-    clima = djsonOW['weather'][0]
-    icon = clima['icon']
-    description = clima['description']
-    return description, icon
+def obtener_clima(lat, lon, appid):
+    try:
+        owUrl = "https://api.openweathermap.org/data/2.5/weather"
+        owParams = {'lat': lat, 'lon': lon, 'appid': appid}
+        owResp = requests.get(url=owUrl, params=owParams)
+        owResp.raise_for_status()
+        djsonOW = owResp.json()
+
+        clima = djsonOW['weather'][0]
+        icon = clima['icon']
+        description = clima['description']
+        return description, icon
+    except requests.exceptions.RequestException as e:
+        # Captura excepciones relacionadas con la solicitud HTTP
+        return "Error de solicitud HTTP:", str(e)
+    except KeyError as e:
+        # Captura excepciones si la estructura JSON no coincide con lo esperado
+        return "Error al procesar la respuesta JSON:", str(e)
+    except Exception as e:
+        # Captura cualquier otra excepción no manejada
+        return "Error desconocido:", str(e)
+
        
 
 '''
-    Función para obtener los climas de dos ciudades.
+    Función para obtener los nombres de las ciudades involucradas en el ticket
 '''
-def obtenerClimas(func, lat1, lon1, lat2, lon2, appid):
-        clima1 = func(lat1, lon1, appid)
-        clima2 = func(lat2, lon2, appid) 
-        return clima1, clima2
-
+def get_nombres( ticket, diccionario):
+    clave = ticket
+     
+    cityO = diccionario[clave]['origin']
+    cityD = diccionario[clave]['destination']
+     
+    return cityO, cityD
 
 
 
@@ -115,14 +127,24 @@ def obtenerClimas(func, lat1, lon1, lat2, lon2, appid):
     La entrada puede er un ticket o el nombre de una ciudad
 '''
 def index(request):
-    entrada, description, icon = None, None, None
+    entrada, description, icon = None, None, None  
     if request.method == 'POST':
         entrada = request.POST.get('city', '')#puede ser un ticket o nombre
         if entrada:
-            appid = readData('data/apiKey.txt')
-            lat, lon = getCoordenadasGC(entrada, appid)
-            description, icon = obtenerClima(lat, lon, appid)
-
-    return render(request, 'weatherApp/index.html', {'entrada': entrada, 
-                                                     'description': description, 
-                                                     'icon': icon })
+            descriptionD, iconD = None, None
+            appid = '7e0007b2bdccf8fd143f738bc8d7644b'
+            ticket = entrada
+            entrada = None
+            diccionario = csv_a_diccionario('data/dataset2.csv')
+            latO, lonO, latD, lonD = get_coordenadas_ds(ticket, diccionario)
+            description, icon = obtener_clima(latO, lonO ,appid) 
+            descriptionD, iconD = obtener_clima(latD, lonD, appid)
+            return render(request, 'weatherApp/index.html', {'entrada': entrada, 
+                                                            'description': description, 
+                                                            'icon': icon,
+                                                            'descriptionD': descriptionD,
+                                                            'iconD': iconD})
+    else:
+        error_message = "Fallo con la entrada"
+        return render(request, 'weatherApp/index.html', {'entrada': entrada, 'error_message': error_message})
+    return render(request, 'weatherApp/index.html', {'entrada': entrada})
