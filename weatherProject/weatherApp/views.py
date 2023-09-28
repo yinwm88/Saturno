3b683e024f5cb9041fbeb8d39e45b9969730637f
wong <yinlen@ciencias.mx>
2023-09-28 09:28:57 -0600

    Se añadioestilo con  css

--- weatherProject/weatherApp/views.py
+++ weatherProject/weatherApp/views.py
@@ -17,9 +17,9 @@ def get_coordenadas_gc(city, appid):
             lon = djsonGC[0]['lon']
             return lat,lon
         else:
-            return city, "No se pudo localizar.", None
+            return city, "No se pudo localizar."
     except requests.exceptions.RequestException as e:
-        return city, "No se encontro el clima.", None
+        return city, "No se encontro el clima."
 
 '''
     Función para obtener el clima de una ciudad.
@@ -32,26 +32,30 @@ def obtener_clima(lat, lon, appid):
         owResp.raise_for_status()
         djsonOW = owResp.json()
         clima = djsonOW['weather'][0]
+        temp = djsonOW['main']['temp']
+        humidity = djsonOW['main']['humidity']
+        pressure = djsonOW['main']['pressure']
+        wind_speed = djsonOW['wind']['speed']
         icon = clima['icon']
         description = clima['description']
-        return description, icon
+        return temp, humidity, pressure, wind_speed, description, icon  
     
     except requests.exceptions.RequestException as e:
         # Captura excepciones relacionadas con la solicitud HTTP
-        return "Error de solicitud HTTP:", str(e)
+        return "Error de solicitud HTTP:", str(e), None, None, None, None
     except KeyError as e:
         # Captura excepciones si la estructura JSON no coincide con lo esperado
-        return "Error al procesar la respuesta JSON:", str(e)
+        return "Error al procesar la respuesta JSON:", str(e), None, None, None, None
     except Exception as e:
         # Captura cualquier otra excepción no manejada
-        return "Error desconocido:", str(e)
+        return "Error desconocido:", str(e), None, None, None, None
 
        
 '''
     Función para mostrar la página de inicio.
 '''
 def index(request):
-    entrada = None
+    entrada, ticket_input, city_input = None, None, None
     if request.method == 'POST':
         entrada = request.POST.get('city', '')#puede ser un ticket o nombre
         if entrada:
@@ -59,24 +63,44 @@ def index(request):
             diccionario = csv_a_diccionario('weatherApp/data/dataset2.csv')
             is_ticket = es_ticket(diccionario, entrada)
             if is_ticket:
+                ticket_input = "Buen viaje! "
                 ticket = entrada
-                entrada = None
+                entrada= None
                 latO, lonO, latD, lonD = get_coordenadas_ds(ticket, diccionario)
-                description, icon = obtener_clima(latO, lonO, appid)
-                descriptionD, iconD = obtener_clima(latD, lonD, appid)
+                tempK, humidity, pressure, wind_speed, description, icon = obtener_clima(latO, lonO, appid)
+                tempKD, humidityD, pressureD, wind_speedD, descriptionD, iconD = obtener_clima(latD, lonD, appid)
+                tempC = round(tempK - 273.15,2)
+                tempCD = round(tempKD - 273.15,2)
                 cityO, cityD = get_nombres(ticket, diccionario)
-                return render(request, 'weatherApp/index.html', {'cityO': cityO,
-                                                                 'description': description, 
+                return render(request, 'weatherApp/index.html', {'ticket_input':ticket_input,
+                                                                 'cityO': cityO,
+                                                                 'tempC':tempC,
+                                                                 'humidity':humidity,
+                                                                 'pressure':pressure,
+                                                                 'wind_speed':wind_speed,
                                                                  'icon': icon,
+                                                                 'description': description, 
                                                                  'cityD':cityD,
-                                                                 'descriptionD': descriptionD,
-                                                                 'iconD': iconD})
+                                                                 'tempCD':tempCD,
+                                                                 'humidityD':humidityD,
+                                                                 'pressureD':pressureD,
+                                                                 'wind_speedD':wind_speedD,
+                                                                 'iconD': iconD,
+                                                                 'descriptionD': descriptionD
+                                                                 })
             else:
+                city_input="Se ingreso una ciudad"
                 city = entrada
-                entrada = None
+                entrada, tempC = None, None
                 lat, lon = get_coordenadas_gc(city, appid)
-                description, icon = obtener_clima(lat, lon, appid)
-                return render(request, 'weatherApp/index.html', {'city':city,
+                tempK, humidity, pressure, wind_speed, description, icon = obtener_clima(lat, lon, appid)
+                tempC = round(tempK - 273.15, 2)
+                return render(request, 'weatherApp/index.html', {'city_input': city_input,
+                                                                 'city':city,
+                                                                 'tempC':tempC,
+                                                                 'humidity':humidity,
+                                                                 'pressure':pressure,
+                                                                 'wind_speed':wind_speed,
                                                                  'description':description,
                                                                  'icon':icon})                
         else:
--- weatherProject/weatherProject/urls.py
+++ weatherProject/weatherProject/urls.py
@@ -1,7 +1,8 @@
 
 from django.urls import path, include
-
+from django.conf import settings
+from django.conf.urls.static import static   
 urlpatterns = [
     path('', include('weatherApp.urls')),
     #cuando alguien acceda al url '' (means example.com for example), con esa direccion va a ir al lugar de weatherApp.urls, o sea que vaya a views y corra index 
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
